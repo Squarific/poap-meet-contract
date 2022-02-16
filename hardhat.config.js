ce803123167d7e217aaecc114061ffe23a987205
Filip Smets <filipsmetsbelgium@gmail.com>
2022-02-16 14:42:08 +0100

    Add contract

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ 
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("./scripts/deploy.js");
+require("./scripts/mint.js");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+ 
+module.exports = {
+  solidity: "0.8.1",
+  defaultNetwork: "ropsten",
+   networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+  },
+};
